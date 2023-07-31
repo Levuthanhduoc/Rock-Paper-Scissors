5646c357ccda7a2124d3050273c2da37ebba3260
levuthanhduoc <levuthanhduoc@gmail.com>
2023-07-31 15:15:25 +0700

    add new ui with user score add reset and start buttion

--- script.js
+++ script.js
@@ -1,28 +1,63 @@
 let computer = document.getElementById("computer");
 let user = document.getElementById("user");
+let score = [0,0];
+const second = 1000;
+function initialSetUp(){
+    enableStarbutton();
+}initialSetUp();
+
+function enableStarbutton(){
+    let start = document.querySelector("#start");
+    start.addEventListener("click",function (){
+        disableUserInterface();
+        startGame();
+        start.textContent= "Reset";
+        score = [0,0];
+        showScore();
+    });
+    start.classList.remove("fading");
+}
+function disableUserInterface(){
+    let keys = document.querySelectorAll("#key");
+    keys.forEach(key => {
+        key.classList.add("fading");
+        let newKey = key.cloneNode(true);
+        key.parentNode.replaceChild(newKey,key);
+    });
+}
+
 /**get computer choice */
 function getComputerChoice() {
     let Ramdom_choice = Math.floor(Math.random()*3);
     switch(Ramdom_choice){
         case 0: 
-                return "rock";
+                return "ROCK";
         case 1: 
-                return "paper";
+                return "PAPER";
         case 2: 
-                return "scissor";
+                return "SCISSOR";
         default:
     }       
+}
+function imageAnimation(location){
+    location.classList.add("scaleUP");
+    setTimeout(function(){
+        location.classList.remove("scaleUP")
+    },second/4);
 }
 /**insert image*/
 function addimage(target,choice){
     switch(choice){
-        case "rock": 
+        case "ROCK":
+            imageAnimation(target);
             target.innerHTML = "<img src=\"Img/rock.svg\"width=\"100px\" height=\"100px\">";
             break;
-        case "paper": 
+        case "PAPER": 
+            imageAnimation(target);
             target.innerHTML = "<img src=\"Img/paper.svg\"width=\"100px\" height=\"100px\">";
             break;
-        case "scissor": 
+        case "SCISSOR": 
+            imageAnimation(target);
             target.innerHTML = "<img src=\"Img/scissor.svg\"width=\"100px\" height=\"100px\">";
             break;
         default:
@@ -30,29 +65,62 @@ function addimage(target,choice){
 }
 /**get user choice */
 /**Compare 2 choice*/
-function getResult(computerChoice,userChoice){
-    let result = document.getElementById("result");
+function showScore(){
+    let scoreLocation = document.querySelectorAll("#score");
+    scoreLocation = Array.from(scoreLocation);
+    scoreLocation[1].textContent = score[1] + ":Player"
+    scoreLocation[0].textContent ="Computer:" + score[0];
+}
+function getScore(computerChoice,userChoice){
     if(computerChoice == userChoice ){
-        result.innerHTML = "TIE";
+        return;
     }
-    else if (computerChoice == "rock" && userChoice == "paper"){
-        result.innerHTML = "PC WIN";
+    else if (computerChoice == "ROCK" && userChoice == "PAPER"){
+        score[1]++;
     }
-    else if (computerChoice == "scissor" && userChoice == "rock"){
-        result.innerHTML = "PC WIN";
+    else if (computerChoice == "SCISSOR" && userChoice == "ROCK"){
+        score[1]++;
     }
-    else if (computerChoice == "paper" && userChoice == "scissor"){
-        result.innerHTML = "PC WIN";
+    else if (computerChoice == "PAPER" && userChoice == "SCISSOR"){
+        score[1]++;
     }
     else {
-        result.innerHTML = "CP WIN";
+        score[0]++;
     }
 }
 /**Run*/
-function startGame(userChoice){
+function RunGame(userChoice){
     let CP = getComputerChoice();
     let PC = userChoice;
     addimage(computer,CP);
     addimage(user,PC);
-    getResult(CP,PC);
+    getScore(CP,PC);
+    showScore();
+    showResult(score);
+    console.log(score);
+}
+
+function startGame(){
+    let keys = document.querySelectorAll("#key");
+    let showStart = document.getElementById("result");
+    keys.forEach(key => {
+        key.classList.remove("fading");
+        key.addEventListener("click",(e)=>{
+            RunGame(key.textContent);
+        });
+    });
+    showStart.textContent = "Let Fight!!!!!!"
+}
+function showResult(score){
+    let result = document.getElementById("result");
+    if(score[1] == 5){
+        result.textContent = "PC is a WINNER!!!!"
+    }
+    else if (score[0] == 5){
+        result.textContent = "CP is a WINNER!!!!"
+    }
+    else{
+        return;
+    }
+    disableUserInterface();
 }
